516dff757650513155d0fbd83b6be799125a1a20
Harshavardhan Potrakonda <63062128+talk2hv@users.noreply.github.com>
2025-07-20 16:01:12 +0000

    variables tweaking

---
+++ Basics/01_variables.js
@@ -0,0 +1,23 @@
+// const accountID = 12
+
+// let accountEmail = "harsha@gmail.com"
+
+// var accountPassword = "12345"
+
+// //accountID = 3
+
+// console.log(accountID);
+
+// accountEmail = "hchchbja"
+// accountPassword = "123456789"
+ 
+// console.log(accountEmail)
+// console.log(accountPassword);
+
+/*
+Do not use var because of issue in block and functional scope.
+*/
+
+let state
+
+console.log(state);
