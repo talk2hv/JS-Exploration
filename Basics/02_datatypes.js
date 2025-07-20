6435e96975a85eab74ec633357c33f8192affaf2
Harshavardhan Potrakonda <63062128+talk2hv@users.noreply.github.com>
2025-07-20 16:23:41 +0000

    datatypes

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all JS code as newer ECMAScript versions
+
+// alert("Hello, World!"); //show a message in the browser, we are using Node.js so this won't work
+
+console.log("Hello, World!");
+
+console.log("Hello, World!"); //show a message in the console
+
