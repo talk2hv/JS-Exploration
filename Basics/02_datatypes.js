e409702ce95e568e3d65149a3c564607a3f004a6
Harshavardhan Potrakonda <63062128+talk2hv@users.noreply.github.com>
2025-07-21 14:42:19 +0000

    datatypes extended

--- Basics/02_datatypes.js
+++ Basics/02_datatypes.js
@@ -1,8 +1,32 @@
 "use strict"; //treat all JS code as newer ECMAScript versions
-
-// alert("Hello, World!"); //show a message in the browser, we are using Node.js so this won't work
-
 console.log("Hello, World!");
 
-console.log("Hello, World!"); //show a message in the console
+console.log(6 + 7);
+console.log("6" + "7"); // string concatenation
+
+let name = "Hasrhavardhan" //string
+
+let age = 21//numbers
+
+//letLoggedIn = true //boolean
+
+//numbers
+//bigInt
+//string
+//boolean -> True or False
+//null -> standalone value
+//undefined
+
+let f
+
+console.log(f)
+console.log(typeof f) //undefined
+
+//symbol - unique
+
+//object
+
+console.log(typeof f)
+
+console.log(typeof string)
 
