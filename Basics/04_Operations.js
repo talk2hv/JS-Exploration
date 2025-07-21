c4bba22f55907b116db3144357538bdcd0e8f413
Harshavardhan Potrakonda <63062128+talk2hv@users.noreply.github.com>
2025-07-21 15:31:22 +0000

    Operations and Assignments

---
+++ Basics/04_Operations.js
@@ -0,0 +1,37 @@
+// let value = 3
+// let negVal = -value
+
+// console.log(value)
+// console.log(negVal); // -3
+
+// console.log(2%3)
+
+// let str1 = "Hello"
+// let str2 = " World"
+
+// let str3 = str1 + str2
+
+// console.log(str3); // Hello World
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+ 
+// console.log("1" + 2 + 3 + "harsha"); // 12
+// console.log(1 + 2 + "harsha"); // 12
+
+console.log(true+false-true+"harsha")
+// console.log(true + false); // 1
+// console.log(true - false); // 1
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 1+3
+
+console.log(num1, num2, num3); // 4 4 4
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); // 101'
+
+
+
