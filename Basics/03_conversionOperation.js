0822d6083a05c741b85f793e724c0593c4e3c56a
Harshavardhan Potrakonda <63062128+talk2hv@users.noreply.github.com>
2025-07-21 15:10:54 +0000

    Conversation of DataTypes

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+// let score = "abcd";
+
+// console.log(typeof score);
+// //console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" -> 33
+//"33abc" -> NaN
+//"abc" -> NaN
+//true -> 1
+//false -> 0
+//null -> 0
+//undefined -> NaN
+
+//Not a Number - NaN
+
+// let isLoggedIn = "";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+//1 - > tRUE, 0 -> fALSE
+//"Ssd" -> true
+//"0" -> true
+//" " -> true
+//"" -> false
+
+// let score = undefined
+
+// if(score){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+let s = 33;
+
+s = String(s)
+
+console.log(s); // 21
+console.log(typeof s); // 21
